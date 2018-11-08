8a327f8fedb882074e0bd1a6921e43d13fc0fc69
Scott Sandre <s.sandre@sap.com>
2018-11-08 15:10:01 -0500

    WIP on following along in helloWorld project #2 using his starter code

--- angular-course/helloWorld3/hello-world/src/app/github-followers/github-followers.component.ts
+++ angular-course/helloWorld3/hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,10 @@
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest'; //depricated
+import 'rxjs/add/operator/map'; //depricated
+import 'rxjs/add/operator/switchMap'; //depricated
 
 @Component({
   selector: 'github-followers',
@@ -9,10 +14,26 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers);
+    // Observable.com
+    Observable.combineLatest([ //combineLatest like this is depricated
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .switchMap(combined => {
+      let paramMapObs = combined[0];
+      let queryParamMapObs = combined[1];
+
+      const id = paramMapObs.get('id');
+      const page = queryParamMapObs.get('page');
+      const order = queryParamMapObs.get('order');
+      console.log(id, page, order);
+      return this.service.getAll();
+    })
+    .subscribe(followers => this.followers = followers );
   }
 }
--- angular-course/helloWorld3/hello-world/src/app/github-profile/github-profile.component.ts
+++ angular-course/helloWorld3/hello-world/src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log('ngOnInit github-profile');
+    this.activatedRoute.paramMap
+      .subscribe(params => {
+        const id: number = +params.get('id'); // the + turns the id string into a number'
+        console.log(id);
+      })
+  }
+
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    })
   }
 
 }
